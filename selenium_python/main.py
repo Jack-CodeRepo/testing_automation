07268ec5be9893193249155c475aa8d849dfe49b
Jack-CodeRepo <devmarboeuf@gmail.com>
2021-07-30 09:28:15 +0200

    factorisation

--- selenium_python/main.py
+++ selenium_python/main.py
@@ -4,8 +4,19 @@ from selenium.webdriver.common.keys import Keys
 
 
 
-def obtenir_un_element_par_son_id(myID):
-    return driver.find_element_by_id(myID)
+# ==================================================================================================
+# Fonctions
+# ==================================================================================================
+
+def aller_sur_une_url(myDriver, myUrl):
+    myDriver.get(myUrl)
+
+def get_driver(myBrowser):
+    if myBrowser == "Firefox":
+        return webdriver.Firefox()
+
+def obtenir_un_element_par_son_id(myDriver, myID):
+    return myDriver.find_element_by_id(myID)
 
 def saisir_du_texte(myElem, myText):
     myElem.send_keys(myText)
@@ -16,19 +27,25 @@ def appuyer_sur_une_touche(myElem, myKey):
 
 
 
-
-
-driver = webdriver.Firefox()
-driver.get("https://www.pagesjaunes.fr/")
+# ==================================================================================================
+# Script
+# ==================================================================================================
 
 sujet = "marboeuf"
 lieu = "35132"
 
-# bypass le block des RGPD en le validant (tout accepter)
-driver.find_element_by_id("didomi-notice-agree-button").click()
+# init du webdriver
+curDriver = get_driver('Firefox')
+# init url testée
+aller_sur_une_url(curDriver, "https://www.pagesjaunes.fr/")
 
-searchBar_01 = obtenir_un_element_par_son_id("quoiqui")
-searchBar_02 = obtenir_un_element_par_son_id("ou")
+# bypass le block des RGPD en le validant (tout accepter)
+curDriver.find_element_by_id("didomi-notice-agree-button").click()
+
+
+searchBar_01 = obtenir_un_element_par_son_id(curDriver, "quoiqui")
+searchBar_02 = obtenir_un_element_par_son_id(curDriver, "ou")
+
 
 saisir_du_texte(searchBar_01, sujet)
 saisir_du_texte(searchBar_02, lieu)
